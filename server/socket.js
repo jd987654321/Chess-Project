c0f86ca398aafbcc239920b16b2d8e27a502676c
jd987654321 <jacobdam06@gmail.com>
2024-11-24 21:09:04 -0500

    added some code for sequelize

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,4 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import express from 'express'
 import http from 'http'
 
@@ -20,24 +20,57 @@ app.get('/', (req, res) => {
 io.on("connection", (socket) => {
     console.log(`New connection: ${socket.id}`)
 
-    let { gameId } = socket.handshake.query
+    let { gameId, playerId } = socket.handshake.query
+    let color;
+
 
     if(!(gameId in games)){
         console.log(`    player 1 has connected to room ${gameId}`)
         let rand = Math.round(Math.random())
         games[gameId] = {
             s1: socket,
-            s2: null,
+            s2: {
+                dummy: '5'
+            },
+            player1Id: playerId,
+            player2Id: null,
             color1: rand ? "white" : "black",
             color2: !rand ? "white" : "black"
         }
-    }else if(!games[gameId].s2){
+        color = games[gameId].color1
+        console.log(games[gameId].player1Id)
+        console.log(playerId)
+    }else if(playerId === games[gameId].player1Id){
+        games[gameId].s1 = socket
+        console.log('player 1 reconnected')
+    }else if('dummy' in games[gameId].s2){
         console.log(`    player 2 has connected to room ${gameId}`)
         games[gameId].s2 = socket
+        games[gameId].player2Id = playerId
+        color = games[gameId].color2
+        console.log(games[gameId].player1Id)
+        console.log(playerId)
+    }else if(playerId === games[gameId].player2Id){
+        games[gameId].s2 = socket
+        console.log('player 2 reconnected')
     }else{
         console.log(`    Game Room full`)
         socket.disconnect(true)
     }
+
+    socket.emit('color', color)
+
+    socket.on('move', (move) => {
+        let opponent = socket === games[gameId].s1 ? games[gameId].s2 : games[gameId].s1;
+        let player = socket === games[gameId].s1 ? "s1" : "s2";
+        console.log(player)
+        opponent.emit('opponentMove', move)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`socket ${socket.id} has disconnected`)
+
+    })
 })
 
 server.listen(8080, () => {
